8fb035c1b4a887e65d11180e66ba7baeeefcb9e5
Indahharahap12 <indahmeilina123@gmail.com>
2023-06-20 18:44:09 +0700

    Tugas2

---
+++ app/views/User/edit.php
@@ -0,0 +1,41 @@
+<h3>
+    Edit Data User
+</h3>
+
+<form action="<?php echo URL; ?>/user/update" method="POST">
+    <input type="hidden" name="user_id" >
+    <div class="mb-3">
+        <label class="form-label">Golongan Id</label>
+        <input type="text" class="form-control" name="user_email" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Nomor</label>
+        <input type="text" class="form-control" name="user_password" required>
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Nama</label>
+        <input type="text" class="form-control" name="user_nama" >
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Alamat</label>
+        <input type="text" class="form-control" name="user_alamat" >
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Hp</label>
+        <input type="text" class="form-control" name="user_hp" >
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Pos</label>
+        <input type="text" class="form-control" name="user_pos" >
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Role</label>
+        <input type="text" class="form-control" name="user_role" >
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Aktif</label>
+        <input type="text" class="form-control" name="user_aktif" >
+    </div>
+        <button type="submit" class="btn btn-success" name="btn_update">Update</button>
+        <a href="<?php echo URL; ?>/user" class="btn btn-primary">Kembali</a>
+</form>
---
+++ app/views/dashboard/index.php
@@ -0,0 +1,9 @@
+
+<div class="card">
+  <div class="card-header">
+    Selamat Datang
+  </div>
+  <div class="card-body">
+    Selamat Datang Di Tugas 2 Pemrograman Berbasis Web Lanjutan Tahun 2023
+  </div>
+</div>
